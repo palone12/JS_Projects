bf45cab121990cf6cde39b0fb91e6b11f47baedd
Pranay Alone <64677840+palone12@users.noreply.github.com>
2023-12-27 13:42:03 +0530

    Color Flipper Project completed

--- colorFlipper/app.js
+++ colorFlipper/app.js
@@ -2,11 +2,13 @@ const colors = ["green", "red", "rgba(133,133,223)", "#f15025"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const headingColor = document.querySelector(".heading");
 
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
+  headingColor.style.color = colors[randomNumber];
 });
 
 function getRandomNumber() {
