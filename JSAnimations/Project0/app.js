313ad018d2554a25afac3aac0b46f664d1b93c66
Pranay Alone <64677840+palone12@users.noreply.github.com>
2024-01-02 16:44:47 +0530

    Project 0 of animation completed with Dark mode

--- JSAnimations/Project0/app.js
+++ JSAnimations/Project0/app.js
@@ -1,4 +1,47 @@
 const moonpath =
-  " M97.5 120C97.5 189.036 190.073 250 118 250C45.9268 250 0.104736 194.742 0.104736 125.706C0.104736 56.6703 58.5316 0.705902 130.605 0.705902C202.678 0.705902 97.5 50.9644 97.5 120Z";
+  "M57 69C57 107.108 117.974 138 76 138C34.0264 138 0 107.108 0 69C0 30.8924 34.0264 0 76 0C117.974 0 57 30.8924 57 69Z";
+
 const sunPath =
-  "M209 100C209 155.228 162.214 200 104.5 200C46.7862 200 0 155.228 0 100C0 44.7715 46.7862 0 104.5 0C162.214 0 209 44.7715 209 100Z";
+  "M152 69C152 107.108 117.974 138 76 138C34.0264 138 0 107.108 0 69C0 30.8924 34.0264 0 76 0C117.974 0 152 30.8924 152 69Z";
+const darkMode = document.querySelector("#darkMode");
+
+let toggle = false;
+
+//toggle function
+
+darkMode.addEventListener("click", () => {
+  console.log("Working");
+  const timeline = anime.timeline({
+    duration: 750,
+    easing: "easeOutExpo",
+  });
+  timeline
+    .add({
+      targets: ".sun",
+      d: [{ value: toggle ? sunPath : moonpath }],
+    })
+    .add(
+      {
+        targets: "#darkMode",
+        rotate: 320,
+      },
+      "-=350"
+    )
+    .add({
+      targets: "section",
+      backgroundColor: toggle ? "rgb(255,255,255)" : "rgb(22,22,22)",
+    })
+    .add(
+      {
+        targets: "h1",
+        color: toggle ? "#000000" : "#ffffff",
+      },
+      "-=700"
+    );
+
+  if (!toggle) {
+    toggle = true;
+  } else {
+    toggle = false;
+  }
+});
